fd4120a5cb83af80e5b469e84434032e288860ce
Humbll <75533134+Humbll@users.noreply.github.com>
2023-09-04 01:30:12 +0200

    more

--- konton.py
+++ konton.py
@@ -1,18 +1,29 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/konton.py', methods=['POST'])
 def register():
-    # Get registration data from the form (similar to your previous code)
+    # Get registration data from the form
     name = request.form.get('name')
     email = request.form.get('email')
     password = request.form.get('password')
 
-    # Save the registration data (similar to your previous code)
+    # Save the registration data to konton.py
+    with open('konton.py', 'a') as file:
+        file.write(f'Name: {name}\n')
+        file.write(f'Email: {email}\n')
+        file.write(f'Password: {password}\n')
+        file.write('\n')  # Add a newline separator
 
-    # Redirect to the index.html page after successful registration
-    return redirect('index.html')
+    # You can add additional logic for database storage or validation here
+
+    # Redirect back to crystal_ball.html after registration
+    return redirect(url_for('crystal_ball'))
+
+@app.route('/konton.html')
+def registration_page():
+    return render_template('konton.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
